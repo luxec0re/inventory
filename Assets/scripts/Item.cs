f5590e09eabbefecc9f7ef0e1db7afb14ac31462
luxec0re <lucas.palmhager@elev.ga.lbs.se>
2024-05-01 21:35:15 +0200

    grid

---
+++ Assets/scripts/Item.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Item : ScriptableObject
+{
+
+    public string itemName;
+    public Sprite Icon;
+}
---
+++ Assets/scripts/ItemSlots.cs
@@ -0,0 +1,9 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlots : MonoBehaviour
+{
+    public Item item;
+    public Image image;
+}
